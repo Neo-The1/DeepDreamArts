aaf35dc556f2318a32e34ec89754230ee96165d3
Neo-The1 <rahul270890@gmail.com>
2019-11-10 19:17:02 -0500

    added to decoder code, need to train

--- train_decoder_for_ust.py
+++ train_decoder_for_ust.py
@@ -13,15 +13,18 @@ import IPython.display
 
 from PIL import Image
 
+import tensorflow.contrib.eager as tfe
 from tensorflow.python.keras.preprocessing import image as k_image
 from tensorflow.python.keras import models
+from tensorflow.python.keras import losses
+from tensorflow.python.keras import layers
+from tensorflow.python.keras import backend as K
 
 #Setup eager execution
 tf.enable_eager_execution()
 print("Eager execution: {}".format(tf.executing_eagerly()))
 
 #set image and content images, include path if not in same directory as this script
-style_img = 'Madhubani.jpg'
 content_img = 'AkankshaNaveen.jpg'
 
 def load_img(img_path):
@@ -49,10 +52,7 @@ def imshow(img, title=None):
 plt.figure(figsize=(10,10))
 content = load_img(content_img)
 style = load_img(style_img)
-plt.subplot(1,2,1)
 imshow(content,'Content Image')
-plt.subplot(1,2,2)    
-imshow(style,'Style Image')
 plt.show()    
 
 def load_and_process_img(raw_img):
@@ -76,7 +76,14 @@ def deprocess_img(img_processed):
     x =np.clip(x,0,255).astype('uint8')
     return x
 
-decoder_layers = ['block1_conv1',
+decoder_layers = ['block5_conv1',
+                'block4_conv1',
+                'block3_conv1', 
+                'block2_conv1', 
+                'block1_conv1'
+               ]
+
+encoder_layers = ['block1_conv1',
                 'block2_conv1',
                 'block3_conv1', 
                 'block4_conv1', 
@@ -95,21 +102,100 @@ def create_model_dcdr():
     # Build model 
     return models.Model(vgg.input, model_outputs)
 
-def get_features(model,init_image):
-    image = load_and_process_img(init_image)
-    
+def create_model_encdr():
+    #load Keras model for vgg19 pre-trained on imagenet data
+    vgg = tf.keras.applications.VGG19(include_top=False)
+    vgg.trainable = False
+    #Get the outputs for required layers
+    # Get output layers corresponding to style and content layers 
+    model_outputs = [vgg.get_layer(name).output for name in encoder_layers]
+    # Build model 
+    return models.Model(vgg.input, model_outputs)
     
 #defining loss as in eq.1 of the paper
-def compute_loss(model_vgg,model_dcdr,init_img,vgg_weight):
-    
+def compute_loss(model_encdr,model_dcdr,init_img,feature_weight):
+    encdr_features = model_encdr(init_img)
+    dcdr_features = model_dcdr(init_img)
+    target_img = dcdr_features[-1]
     pixel_loss = tf.reduce_mean(tf.square(init_img - target_img))
-    encdr_features = model_vgg(init_img);
-    dcdr_features = model_dcdr(init_img);
     #feature loss
-    for layer in vgg_init:
-        feature_loss
+    feature_loss = 0
+    for encdr_layer,dcdr_layer in zip(encdr_features,dcdr_features):
+        feature_loss += tf.reduce_mean(tf.square(encdr_layer[0]-dcdr_layer[0]))
     
+    return pixel_loss+feature_weight*feature_loss
     
+def train_decoder(init_image,feature_weight=1,num_iterations = 1000):
+    encdr = create_model_encdr()
+    dcdr = create_model_dcdr()
     
+    for layer in encdr.layers:
+        layer.trainable = False
+        
+    for layer in dcdr.layers:
+        layer.trainable = True
+        
+    init_img = load_and_process_img(init_image)
+    init_img = tfe.Variable(init_img,dtype=tf.float32)
+
+    #create optimizer
+    optimizer = tf.train.AdamOptimizer(learning_rate=5, beta1=0.99, epsilon=0.1)
+    
+    #store the best result
+    best_loss, best_img = float('inf'), None
+    
+    #For display
+    num_rows = 2
+    num_cols = 5
+    display_interval = num_iterations/(num_rows*num_cols)
+    start_time = time.time()
+    global_start_time = time.time()
+    
+    norm_means = np.array([103.939, 116.779, 123.68])
+    min_vals = -norm_means
+    max_vals = 255 - norm_means
+    
+    imgs = []
+    
+    for i in range(num_iterations):
+        loss = compute_loss(encdr,dcdr,init_img,feature_weight)
+        clipped = []
+        #clip image values
+        for ii in range(3):
+            clipped.append(tf.clip_by_value(init_img[:,:,:,ii], min_vals[ii], max_vals[ii]))
+            
+        init_img.assign(tf.stack(clipped,axis=-1))
+        
+        if loss < best_loss:
+            #Update best loss and best image
+            best_loss = loss
+            #.numpy() gives concrete array
+            best_img = deprocess_img(init_img.numpy())
+            
+        if i % display_interval==0:
+            start_time = time.time()
+            plot_img = init_img.numpy()
+            plot_img = deprocess_img(plot_img)
+            imgs.append(plot_img)
+            IPython.display.clear_output(wait = True)
+            IPython.display.display_png(Image.fromarray(plot_img))
+            print('Iteration: {}'.format(i))
+            print('Total Loss: {:.4e},'
+                  'Time: {:.4f}s'.format(loss,time.time()-start_time))
+            
+    print('Total Time: {:.4f}s'.format(time.time()-global_start_time))
+    IPython.display.clear_output(wait=True)
+    plt.figure(figsize=(14,4))
+    
+    for i,img in enumerate(imgs):
+        plt.subplot(num_rows,num_cols,i+1)
+        plt.imshow(img)
+        plt.xticks([])
+        plt.yticks([])
+        
+    return best_img, best_loss
+
+
+
 
     
